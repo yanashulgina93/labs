2000bcb4b7387d5361aa3e0ff87eaf4045beb486
yanashulgina93 <yana_shulgina93@mail.ru>
2016-07-14 16:48:17 +0300

    empty main

--- main.cpp
+++ main.cpp
@@ -1,33 +1,6 @@
-#include "myFilter.hpp"
-#include "highgui.h"
-#include <iostream>
-void testMyFilter(const cv::Mat & src)
-{
-    int threshold1 = 50;
-    int threshold2 = 120;
-    int ksize = 4;
-    cv::Mat filtered(src.rows, src.cols, CV_8UC3);
-
-    myFilter(src, filtered, threshold1, threshold2, ksize);
-
-
-
-    cv::imshow("src", src);
-    cv::imshow("filtered", filtered);
-    cv::waitKey(0);
-}
-
 
 int main()
 {
-    cv::Mat src = cv::imread("../door.png", 1);
-    if (src.empty())
-    {
-        std::cout<<"Can not read image"<<std::endl;
-        return -1;
-    }
-
-    testMyFilter(src);
 
     return 0;
 }
